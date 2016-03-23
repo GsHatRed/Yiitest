d66e02fcf3e64ec2ba7765acee942622fc2f100f
GsHatRed <306655621@qq.com>
2016-03-23 18:09:15 +0800

    name

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'have a try',
+	'name'=>'Gself Blog',
 	'charset' => 'UTF-8',
     'language'=>'zh_CN',
 	// preloading 'log' component
